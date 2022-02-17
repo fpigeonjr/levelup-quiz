88b0bdfda5f92f38235185fd302d72c76f1c6138
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-17 16:53:49 -0600

    adds binding

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,8 +1,19 @@
 <script>
-  export let quizName = "Frank Quiz"
+  export let quizName = 'Frank Quiz'
+  let title = ''
+  let a = 0
+  let b = 0
+
+  const add = () => a + b
 </script>
 
 <div>
-  <h2>{quizName} </h2>
-  <input type="text" />
+  <h2>{quizName}</h2>
+  <p>{title}</p>
+  <input bind:value={title} type="text" />
+  <br />
+  <input type="number" bind:value={a} />
+  <input type="number" bind:value={b} />
+  <p>Sum</p>
+  <p>Total is {add(a, b)}</p>
 </div>
