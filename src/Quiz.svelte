f9995ff74584cfc53cc849e40d5109f1a9c727ef
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-17 17:16:58 -0600

    basic events

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -3,8 +3,15 @@
   let title = ''
   let a = 0
   let b = 0
-
+  let result = ''
+  let correctAnswer = 'b'
   const add = () => a + b
+  const pickAnswer = (value) => {
+    if (value === correctAnswer) {
+      return (result = 'Correct')
+    }
+    result = 'Oops, wrong answer'
+  }
 </script>
 
 <div>
@@ -16,4 +23,9 @@
   <input type="number" bind:value={b} />
   <p>Sum</p>
   <p>Total is {add(a, b)}</p>
+  <h4>{result}</h4>
+  <button on:click={() => pickAnswer('a')}>Answer A</button>
+  <button on:click={() => pickAnswer('b')}>Answer B</button>
+  <button on:click={() => pickAnswer('c')}>Answer C</button>
+  <button on:click={() => pickAnswer('d')}>Answer D</button>
 </div>
